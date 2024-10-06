c06689b16be8c10db15e78576c646470942158a6
viktorortelan <jvortelan@gmail.com>
2024-10-06 00:09:27 -0300

    subido

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import './index.scss';
+import { Link } from 'react-router-dom';
+import axios from 'axios'
+
+export default function Home() {
+
+    return(
+        <div className="home">
+            <h1>oii keeh bleeeee</h1>
+        </div>
+    )
+}
